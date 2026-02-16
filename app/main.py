eb0d4b24fc172c78d1cf2941c42f40207398ed52
DorianQA-WEB <qa.doriangrey@gmail.com>
2026-02-16 16:59:38 +0500

    GRAFANA

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, HTTPException
 from fastapi.responses import Response
-from prometheus_client import Counter, generate_latest
+from prometheus_client import Counter, generate_latest, Gauge, Histogram
+from pydantic import BaseModel
+import time
 
 app = FastAPI()
 
@@ -11,11 +13,50 @@ REQUESTS_TOTAL = Counter(
     ["method", "endpoint", "status_code"]
 )
 
+ACTIVE_CONNECTIONS = Gauge(
+    "active_connections",
+    "Current number of active connections",
+    ["app"]
+)
+
+REQUEST_DURATION = Histogram(
+    "http_request_duration_seconds",
+    "HTTP request duration in seconds",
+    ["method", "endpoint"],
+    buckets=[0.1, 0.3, 0.5, 1.0, 2.0, 5.0]
+)
+
+# Модель для POST-запроса
+class UserCreate(BaseModel):
+    username: str
+
 
 @app.get("/users/")
 async def get_users():
-    REQUESTS_TOTAL.labels(method="GET", endpoint="/users/", status_code=200).inc()
-    return {"message": "List of users"}
+    ACTIVE_CONNECTIONS.labels(app="fastapi").inc()
+    start_time = time.time()
+    try:
+        REQUESTS_TOTAL.labels(method="GET", endpoint="/users/", status_code=200).inc()
+        REQUEST_DURATION.labels(method="GET", endpoint="/users/").observe(time.time() - start_time)
+        return {"message": "List of users"}
+    finally:
+        ACTIVE_CONNECTIONS.labels(app="fastapi").dec()
+
+
+@app.post("/users/")
+async def create_user(user: UserCreate, request: Request):
+    ACTIVE_CONNECTIONS.labels(app="fastapi").inc()
+    start_time = time.time()
+    try:
+        if len(user.username) < 3:
+            REQUESTS_TOTAL.labels(method="POST", endpoint="/users/", status_code=400).inc()
+            REQUEST_DURATION.labels(method="POST", endpoint="/users/").observe(time.time() - start_time)
+            raise HTTPException(status_code=400, detail="Username is too short")
+        REQUESTS_TOTAL.labels(method="POST", endpoint="/users/", status_code=201).inc()
+        REQUEST_DURATION.labels(method="POST", endpoint="/users/").observe(time.time() - start_time)
+        return {"message": "User created", "username": user.username}
+    finally:
+        ACTIVE_CONNECTIONS.labels(app="fastapi").dec()
 
 @app.get("/metrics")
 async def metrics():
